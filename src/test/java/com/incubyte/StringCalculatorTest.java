11ef2b9bdf534d4ec23775c8379117692aef030f
Jeet Bhuptani <jeet.work.id@gmail.com>
2025-07-03 15:29:08 +0530

    Step 2 Allowed any amount of Numbers

--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -5,20 +5,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+    // Step 1.1 Empty String
     @Test
     void returnsZeroOnEmptyString() {
         assertEquals(0, new StringCalculator().add(""));
     }
 
+    // Step 1.2 Single Integer Value
     @Test
     void returnsNumberForSingleInput(){
         assertEquals(5, new StringCalculator().add("5"));
     }
 
+    // Step 1.3 Comma separated values
     @Test
     void returnsSumofNumbersSeperatedByComma() {
         assertEquals(6, new StringCalculator().add("1,5"));
         assertEquals(10, new StringCalculator().add("1,2,3,4"));
     }
 
+    // Step 2 - Allow any amount of numbers
+    @Test
+    void handlesLargeSumWithoutOverflow() {
+        StringBuilder input = new StringBuilder();
+        for(int i = 0; i< 2_000_000; i++)
+            input.append("1,");
+        input.append("1");
+        assertEquals(2_000_001L, new StringCalculator().add(input.toString()));
+    }
 }
