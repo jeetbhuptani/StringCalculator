f8c9ea49432d9229734ba16feffc0810b2d9dc44
Jeet Bhuptani <jeet.work.id@gmail.com>
2025-07-03 15:08:55 +0530

    Initial Setup and Step 1.1: Return 0 for empty String input

---
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.incubyte;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+    @Test
+    void returnsZeroOnEmptyString() {
+        assertEquals(0, new StringCalculator().add(""));
+    }
+}
