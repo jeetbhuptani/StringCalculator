e9b0ce64793aee1400893978b8738cbe0a3282cb
Jeet Bhuptani <jeet.work.id@gmail.com>
2025-07-03 15:19:09 +0530

    Step 1.3 Return sum of numbers with comma separated values

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -6,7 +6,13 @@ public class StringCalculator {
         //Step 1.1 - Return 0 for Empty Values
         if(numbers.isEmpty()) return 0;
 
-        //Step 1.2 - Return number for Given String with Single Number
-        return Integer.parseInt(numbers);
+        //Step 1.2 - Return number for given string with Single Number
+        //Step 1.3 - Return sum of comma separate values
+        String[] parts = numbers.split(",");
+        int sum = 0;
+        for(String part: parts){
+            sum += Integer.parseInt(part);
+        }
+        return sum;
     }
 }
--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -8,6 +8,17 @@ public class StringCalculatorTest {
     @Test
     void returnsZeroOnEmptyString() {
         assertEquals(0, new StringCalculator().add(""));
+    }
+
+    @Test
+    void returnsNumberForSingleInput(){
         assertEquals(5, new StringCalculator().add("5"));
     }
+
+    @Test
+    void returnsSumofNumbersSeperatedByComma() {
+        assertEquals(6, new StringCalculator().add("1,5"));
+        assertEquals(10, new StringCalculator().add("1,2,3,4"));
+    }
+
 }
